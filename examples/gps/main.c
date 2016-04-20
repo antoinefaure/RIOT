9e742fba32b7a680b8197b6d64c67176a4221e12
Antoine Faure <antoine.faure@openwide.fr>
2016-04-20 19:44:46 +0200

    Kickoff gps

---
+++ examples/gps/main.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <periph/uart.h>
+
+int checksum(char *s) {
+int c = 0;
+while(*s)
+c ^= *s++;
+return c;
+}
+
+void getData (void *arg, uint8_t data)
+{
+    printf("%d", data);
+}
+
+
+int main(void)
+{
+    puts("Hello World!");
+
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+    int ret = uart_init(UART_DEV(UART_1), 9600, getData, NULL);
+    printf("ret = %d\n", ret);
+    while(1);
+    return 0;
+}
